f417dd2695979543f5d51846c1e046c465048ab2
Kyoo130 <kml9003@kakao.com>
2021-10-04 10:57:30 +0900

    [update] 서버 업데이트

--- 02_week_deep/image-community/src/shared/firebase.js
+++ 02_week_deep/image-community/src/shared/firebase.js
@@ -5,14 +5,14 @@ import "firebase/compat/auth";
 import "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCbFNpene1yEg21kOLGb7LWNzTmgTf-orA",
-    authDomain: "image-community-64a78.firebaseapp.com",
-    projectId: "image-community-64a78",
-    storageBucket: "image-community-64a78.appspot.com",
-    messagingSenderId: "20739123014",
-    appId: "1:20739123014:web:343cf9b0dc9364fba96747",
-    measurementId: "G-L4EW3RZHNW"
-};
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASURMENT_ID
+  };
 
 firebase.initializeApp(firebaseConfig);
 

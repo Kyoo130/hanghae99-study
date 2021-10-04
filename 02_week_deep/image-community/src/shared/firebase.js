406e350e4c0425023f9586ff12059a6139478339
Kyoo130 <kml9003@kakao.com>
2021-10-04 10:54:39 +0900

    [update] 서버 업데이트

--- 02_week_deep/image-community/src/shared/firebase.js
+++ 02_week_deep/image-community/src/shared/firebase.js
@@ -1,14 +1,22 @@
-// import firebase from "firebase/app";
-// import "firebase/auth";
-
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 
+// import firebase from "firebase/app";
+import "firebase/auth";
+
 const firebaseConfig = {
-// 정보 숨김
+    apiKey: "AIzaSyCbFNpene1yEg21kOLGb7LWNzTmgTf-orA",
+    authDomain: "image-community-64a78.firebaseapp.com",
+    projectId: "image-community-64a78",
+    storageBucket: "image-community-64a78.appspot.com",
+    messagingSenderId: "20739123014",
+    appId: "1:20739123014:web:343cf9b0dc9364fba96747",
+    measurementId: "G-L4EW3RZHNW"
 };
 
 firebase.initializeApp(firebaseConfig);
 
+const apiKey = firebaseConfig.apiKey;
 const auth = firebase.auth();
-export { auth };
+
+export{auth, apiKey};
